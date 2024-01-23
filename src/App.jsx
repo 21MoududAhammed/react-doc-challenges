bb0a58a0d144c45f7d33e92d4b625f91a5a28338
21MoududAhammed <21moudud@gmail.com>
2024-01-23 06:56:53 +0600

    chellange or problem 2 of state structure has done

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,68 @@
-import { useState, useEffect } from 'react';
-import Clock from './components/Clock';
-// import Clock from './../src/components/Index.jsx';
+import { useState } from "react";
+import AddItem from "./components/AddItem";
+import PackingList from "./components/PakingList";
 
-function useTime() {
-  const [time, setTime] = useState(() => new Date());
-  useEffect(() => {
-    const id = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-    return () => clearInterval(id);
-  }, []);
-  return time;
-}
 
-export default function App() {
-  const time = useTime();
-  const [color, setColor] = useState('lightcoral');
+const initialItems = [
+  { id: 0, title: "Warm socks", packed: true },
+  { id: 1, title: "Travel journal", packed: false },
+  { id: 2, title: "Watercolors", packed: false },
+];
+
+export default function TravelPlan() {
+  const [items, setItems] = useState(initialItems);
+
+  const [packed, setPacked] = useState(1);
+
+  // onAddItem
+  function handleAddItem(title) {
+    setItems([
+      ...items,
+      {
+        id: items.length,
+        title: title,
+        packed: false,
+      },
+    ]);
+  }
+
+  function handleChangeItem(nextItem) {
+    if (nextItem.packed) {
+      setPacked(packed + 1);
+    } else {
+      setPacked(packed - 1);
+    }
+    setItems(
+      items.map((item) => {
+        if (item.id === nextItem.id) {
+          return nextItem;
+        } else {
+          return item;
+        }
+      })
+    );
+  }
+
+  function handleDeleteItem(itemId) {
+    const deletedItem = items.find((item) => item.id === itemId);
+    if (deletedItem.packed) {
+      setPacked(packed - 1);
+    }
+    setItems(items.filter((item) => item.id !== itemId));
+  }
+
   return (
-    <div>
-      <p>
-        Pick a color:{' '}
-        <select value={color} onChange={e => setColor(e.target.value)}>
-          <option value="lightcoral">lightcoral</option>
-          <option value="midnightblue">midnightblue</option>
-          <option value="rebeccapurple">rebeccapurple</option>
-        </select>
-      </p>
-      <Clock color={color} time={time.toLocaleTimeString()} />
-    </div>
+    <>
+      <AddItem onAddItem={handleAddItem} />
+      <PackingList
+        items={items}
+        onChangeItem={handleChangeItem}
+        onDeleteItem={handleDeleteItem}
+      />
+      <hr />
+      <b>
+        {packed} out of {items.length} packed!
+      </b>
+    </>
   );
 }
